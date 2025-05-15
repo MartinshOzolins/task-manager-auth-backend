6d84f6bdc1f2bbe9b3d7fcb5d0ea2d1b9de49afd
Martins Ozolins <ozolins.martinsh@gmail.com>
2025-05-15 22:04:20 +0300

    change server start file from server.js to app.js

--- server.ts
+++ server.ts
@@ -1,10 +1,9 @@
-import express from "express";
 import dotenv from "dotenv";
+
+import app from "./app.js";
 
 // Loads env variables into process.env
 dotenv.config();
-
-const app = express();
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server running on port ${process.env.PORT || 3000}`);
