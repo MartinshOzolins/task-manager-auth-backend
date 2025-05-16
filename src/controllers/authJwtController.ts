f4f8f24ea0a67467a3f4b0136d528db9f6bcd168
Martins Ozolins <ozolins.martinsh@gmail.com>
2025-05-17 01:13:14 +0300

    add initial (unfinished) registration controller function for jwt auth

--- src/controllers/authJwtController.ts
+++ src/controllers/authJwtController.ts
@@ -1,3 +1,24 @@
-export async function login() {}
+import { Request, Response } from "express";
+import { signToken } from "../utils/helpers.js";
 
-export async function register({ email, password }) {}
+export async function register(req: Request, res: Response) {
+  // retrieve inputs
+  const data = req.body;
+  const email = data.email || "";
+  const password = data.password || "";
+
+  // validate inputs
+  if (!email || !password) {
+    throw new Error("Please provide password and email");
+  }
+  // create jwt
+  const token = signToken("123");
+
+  // attach jwt cookie
+  res.cookie("jwt", token);
+
+  res.json("Registration succesful");
+  // return success message
+}
+
+export async function login() {}
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,12 +1,5 @@
 import bcrypt from "bcrypt";
-import {
-  JsonWebTokenError,
-  Jwt,
-  JwtHeader,
-  JwtPayload,
-  sign,
-  verify,
-} from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 const saltRounds = 10;
 
@@ -22,4 +15,37 @@ export async function comparePassword(
   storedPassword: string
 ) {
   const match = await bcrypt.compare(currentPassword, storedPassword);
+  if (match) return match;
+  else throw new Error("Invalid credentials, incorrect password or email");
+}
+
+export async function signToken(userId: string) {
+  const token = await new Promise((resolve, reject) => {
+    jwt.sign(
+      { userId: userId },
+      `${process.env.JWT_SECRET}`,
+      { expiresIn: "2d" },
+      // callback function
+      (err, token) => {
+        if (err) return reject(err);
+        else resolve(token);
+      }
+    );
+  });
+  return token;
+}
+
+export async function verifyToken(token: string) {
+  const decoded = await new Promise((resolve, reject) => {
+    jwt.verify(
+      token,
+      `${process.env.JWT_SECRET}`,
+      // callback function
+      (err, decoded) => {
+        if (err) return reject(err);
+        else resolve(decoded);
+      }
+    );
+  });
+  return decoded;
 }
